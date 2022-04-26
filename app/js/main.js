9af73484bf41cda1f7f402a59c58d74b5823df0b
Arkadiusz Szewczyk <a.szewczyk1997@wp.pl>
2022-04-26 19:28:23 +0100

    Repair h1 and button place in section-2 after reach 1024px

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,15 @@
+// const header = document.querySelector(".header"); Implement sticky nav
 const openMobileMenu = document.querySelector(".header__hamburger");
 const closeMobileMenu = document.querySelector(".header__close");
 
 const activeMenu = function (val, val2) {
   this.val = val;
   this.val2 = val2;
+
+  // header.scrollIntoView({  Implement sticky nav
+  //   behavior: "smooth",
+  // });
+
   document.querySelector(
     ".header__nav--mobile"
   ).style.transform = `translateX(${val}%)`;
